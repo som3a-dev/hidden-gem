bf07427ff83df7e27025a26b01f65569ad049c3b
flex <142276723+faleqas@users.noreply.github.com>
2025-12-09 18:59:54 +0200

    Update game.cpp

--- sources/game.cpp
+++ sources/game.cpp
@@ -2,6 +2,7 @@
 #include "gameplay_systems.h"
 
 #include <iostream>
+#include <utility>
 
 void Game::init()
 {
@@ -9,16 +10,20 @@ void Game::init()
     screen_height = 720;
     
     InitWindow(screen_width, screen_height, "Hidden GEM");
-    SetTargetFPS(60);
-
+    SetTargetFPS(70);
+    
     asset_m.load_texture(ASSETS_PATH"knight/knight000.png");
-    asset_m.print_texture_table();
     asset_m.load_texture(ASSETS_PATH"knight/knight001.png");
-    asset_m.print_texture_table();
     asset_m.load_texture(ASSETS_PATH"knight/knight002.png");
+
     asset_m.print_texture_table();
 
-    create_player();
+    create_player(0, 0);
+
+/*    for (int i = 1000; i < 2000; i++)
+    {
+        create_player(static_cast<float>(i * 4), static_cast<float>(i * 2));
+    }*/
 }
 
 void Game::destroy()
@@ -26,11 +31,11 @@ void Game::destroy()
     CloseWindow();
 }
 
-void Game::create_player()
+void Game::create_player(float x, float y)
 {
-    ECS::TransformComponent transform = {50, 50, 256, 256};
+    ECS::TransformComponent transform = {x, y, 256, 256};
     ECS::MovementComponent movement;
-    movement.speed = 4.0f;
+    movement.speed = 0.3f;
 
     ECS::DrawableComponent drawable;
     drawable.texture_path = ASSETS_PATH"knight/knight002.png";
@@ -48,11 +53,11 @@ void Game::create_player()
     ECS::PlayerComponent player_tag;
 
     player = world.create_entity();
-    world.transforms.add_component(player, transform);
-    world.movements.add_component(player, movement);
-    world.drawables.add_component(player, drawable);
-    world.animated_drawables.add_component(player, animated_drawable);
-    world.players.add_component(player, player_tag);
+    world.transforms.add_component(player, std::move(transform));
+    world.movements.add_component(player, std::move(movement));
+    world.drawables.add_component(player, std::move(drawable));
+    world.animated_drawables.add_component(player, std::move(animated_drawable));
+    world.players.add_component(player, std::move(player_tag));
 }
 
 void Game::loop()
@@ -73,8 +78,10 @@ void Game::update()
         return;
     }
 
+    dt = GetFrameTime() * 1000;
+
     using namespace GameplaySystems;
-    player_system(world);
+    player_system(world, dt);
 
     transform_update_system(world);
     animated_drawable_system(world);
@@ -84,9 +91,14 @@ void Game::draw()
 {
     BeginDrawing();
 
-    ClearBackground(RAYWHITE);
+    ClearBackground({60, 60, 60, 255});
 
     GameplaySystems::render_drawable_system(world, asset_m);
+
+    int fps = GetFPS();
+    char buf[12];
+    snprintf(buf, sizeof(buf), "%d", fps);
+    DrawText(buf, 0, 0, 48, RED);
 
     EndDrawing();
 }
