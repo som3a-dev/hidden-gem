72682ef515a60b81bee67878c9a893823c9f576c
som3a-dev <omar.eltayeb54@gmail.com>
2026-02-21 01:05:34 +0200

    move question panel text drawing from popup image

--- source/ui/question_panel.cpp
+++ source/ui/question_panel.cpp
@@ -4,6 +4,52 @@
 #include <assert.h>
 #include <iostream>
 
+static void draw_boxed_text(Game* game, const Font& font, const std::string& text, Vector2 text_pos, int textsz, Rectangle box, float spacing, Color color)
+{
+    int words_count;
+    char** words = TextSplit(text.c_str(), ' ', &words_count);
+
+    float char_x = text_pos.x;
+    float char_y = text_pos.y;
+    for (int i = 0; i < words_count; i++)
+    {
+        const char* word = words[i];
+        Vector2 word_sz = MeasureTextEx(font, word, (float)textsz, spacing);
+
+        if ((char_x + word_sz.x) >= (box.x + box.width))
+        {
+            char_x = text_pos.x;
+            char_y += word_sz.y;
+        }
+
+        if ((char_y + word_sz.y) >= (box.y + box.height))
+        {
+
+        }
+
+        for (int j = 0; j < strlen(word); j++)
+        {
+//            DrawTextCodepoint(font, word[j], {char_x, char_y}, (float)textsz, color);
+            char chars[2] = {word[j], '\0'};
+            game->QueueTextEx(font, chars, {char_x, char_y}, (float)textsz, spacing, color);
+
+            Vector2 size = MeasureTextEx(font, chars, (float)textsz, 0);
+            char_x += size.x + spacing;
+
+            if ((char_x + size.x) >= (box.x + box.width))
+            {
+                char_x = text_pos.x;
+                char_y += size.y;
+            }
+        }
+
+        char chars[] = {' ', '\0'};
+        Vector2 size = MeasureTextEx(font, chars, (float)textsz, 0);
+        char_x += size.x;
+    }
+}
+
+
 namespace UI
 {
     QuestionPanel::QuestionPanel()
@@ -115,6 +161,33 @@ namespace UI
         for (const Button& button : buttons)
         {
             button.draw(game, font);
+        }
+
+        if (background.state == PopupImageState::Shown)
+        {
+            Rectangle rect = background.rect;
+            Color color;
+            color.r = 0x12;
+            color.g = 0x11;
+            color.b = 0x0f;
+            color.a = 0xff;
+
+            const float spacing = 1;
+
+            Vector2 text_pos = {rect.x + 4, rect.y + 4};
+
+            game->QueueTextEx(font, "Mission 1: ", text_pos, 20, spacing, color);
+
+            Vector2 body_pos = {text_pos.x, text_pos.y + 36};
+            const std::string body = "The next scroll is hidden inside the Pharaoh`s chariot. Can you uncover How many spokes does each wheel have, and how does this design help?";
+
+            draw_boxed_text(game, font, body, body_pos,
+            16,
+            {
+                body_pos.x, body_pos.y,
+                rect.width, rect.height
+            },
+            spacing, color);
         }
     }
 
